fef3df05bc26a4123ea3eee9a89c501c1f39db74
ShaniGeva12 <shanigeva12@gmail.com>
2024-04-16 11:33:27 +0300

    packages updated & merged from 3 projects

--- src/styles/material/theme.tsx
+++ src/styles/material/theme.tsx
@@ -5,15 +5,15 @@ import { Theme, createTheme } from "@mui/material";
 // export const ltrDir: Direction = 'ltr';
 
 export const darkTheme: Theme = createTheme({
-    palette: {
-      mode: 'dark',
-    },
-    // direction: rtlDir,
-  });
+  palette: {
+    mode: 'dark',
+  },
+  // direction: rtlDir,
+});
 
-  export const lightTheme: Theme = createTheme({
-    palette: {
-      mode: 'light',
-    },
-    // direction: rtlDir,
-  });
+export const lightTheme: Theme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+  // direction: rtlDir,
+});
